8b6427766194a5de7a8a303b00ff509ea64e7039
TheElenwe <elenwe97@hotmail.com>
2024-03-08 13:03:06 +0200

    added cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,22 @@
-import Button from "./UI/Button";
-import logoImg from "../assets/logo.jpg";
-import { useContext } from "react";
-import CartContext from "../store/CartContext";
+import { useContext } from 'react';
+
+import Button from './UI/Button.jsx';
+import logoImg from '../assets/logo.jpg';
+import CartContext from '../store/CartContext.jsx';
+import UserProgressContext from '../store/UserProgressContext.jsx';
 
 export default function Header() {
-  const cartContext = useContext(CartContext);
+  const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
 
-  const totalCartItems = cartContext.items.reduce((totalNumberOfItems, item) => {
-    return totalNumberOfItems + item.quantity
-  }, 0)
+  const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
+    return totalNumberOfItems + item.quantity;
+  }, 0);
+
+  function handleShowCart() {
+    userProgressCtx.showCart();
+  }
+
   return (
     <header id="main-header">
       <div id="title">
@@ -16,7 +24,9 @@ export default function Header() {
         <h1>ReactFood</h1>
       </div>
       <nav>
-        <Button textOnly>Cart ({totalCartItems})</Button>
+        <Button textOnly onClick={handleShowCart}>
+          Cart ({totalCartItems})
+        </Button>
       </nav>
     </header>
   );
